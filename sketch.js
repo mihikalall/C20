013834a3e2a350f71c00617d727fc7b4d1d80203
mihikalall <67033899+mihikalall@users.noreply.github.com>
2020-06-21 13:46:44 -0400

    Designing Algorithms

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixedRect
+var movingRect
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(200, 200, 50, 50);
+  fixedRect.shapeColor = "green";
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+}
+
+function draw() {
+  background(0,0,0);
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  //so the rectangles only change color when they touch the middle rectangle
+  console.log (fixedRect.x - movingRect.x);
+  console.log (fixedRect.width/2 + movingRect.width/2);
+  console.log(fixedRect.y - movingRect.y);
+  console.log(fixedRect.height/2 + movingRect.height/2)
+//condtion for the rectangle to change color when the moving rectangle touches it
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+  && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+  && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.width/2
+  && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2 ) {
+  fixedRect.shapeColor = "red";
+  movingRect.shapeColor = "red";
+}
+else {
+  fixedRect.shapeColor = "green";
+  movingRect.shapeColor = "green";
+}
+  drawSprites();
+}
